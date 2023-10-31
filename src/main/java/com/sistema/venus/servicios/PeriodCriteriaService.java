3ea186966308ebfb9991b7138c14391d68871547
Sebastian Quiros <squirosv@ucenfotec.ac.cr>
2023-10-31 13:58:52 -0600

    timestamp

--- src/main/java/com/sistema/venus/servicios/PeriodCriteriaService.java
+++ src/main/java/com/sistema/venus/servicios/PeriodCriteriaService.java
@@ -6,12 +6,15 @@ import com.sistema.venus.repo.PeriodCriteriaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
+
 @Service
 public class PeriodCriteriaService {
     @Autowired
     private PeriodCriteriaRepository periodCriteriaRepository;
 
     public PeriodCriteria createPeriodCriteria(PeriodCriteria periodCriteria){
+        periodCriteria.setDate(LocalDate.now());
         return periodCriteriaRepository.save(periodCriteria);
     }
 
