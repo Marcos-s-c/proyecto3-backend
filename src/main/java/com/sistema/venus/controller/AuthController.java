ae371a4ab10f4b3fabdfafbb10cf7cb885f7fc0b
Marcos-s-c <66282209+Marcos-s-c@users.noreply.github.com>
2023-10-31 23:34:05 -0600

    added @CrossOrigin("*") to AuthController for FE compatability

--- src/main/java/com/sistema/venus/controller/AuthController.java
+++ src/main/java/com/sistema/venus/controller/AuthController.java
@@ -9,39 +9,52 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.BadCredentialsException;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/rest/auth")
+@CrossOrigin("*")
 public class AuthController {
     @Autowired
     private UserService userService;
 
     @Autowired
     private AuthService authService;
+
     @PostMapping(value = "login")
-    public ResponseEntity login(@RequestBody LoginRequest loginRequest)  {
+    public ResponseEntity<Object> login(@RequestBody LoginRequest loginRequest) {
 
         try {
+            // Autenticar al usuario
             LoginResponse loginRes = authService.getToken(loginRequest);
 
-            return ResponseEntity.ok(loginRes);
+            // Cargar información adicional del usuario
+            UserDetails userDetails = userService.loadUserByUsername(loginRequest.getEmail());
 
-        }catch (BadCredentialsException e){
+            // Combina la respuesta de inicio de sesión con los detalles del usuario en un mapa
+            Map<String, Object> responseMap = new HashMap<>();
+            responseMap.put("token", loginRes.getToken());
+            responseMap.put("user", userDetails);
+
+            return ResponseEntity.ok(responseMap);
+
+        } catch (BadCredentialsException e) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(HttpStatus.BAD_REQUEST);
-        }catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
             throw e;
         }
     }
+
     @PostMapping(value = "register")
-    public ResponseEntity register(@RequestBody User user)  {
+    public ResponseEntity register(@RequestBody User user) {
         try {
             return ResponseEntity.ok(userService.createUser(user));
-        }catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
             throw e;
         }
