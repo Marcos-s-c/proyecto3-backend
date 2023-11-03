e1c17b0aec9cef789cc15e034d0be4901d94065c
Sebastian Quiros <squirosv@ucenfotec.ac.cr>
2023-11-02 18:54:48 -0600

    sendgrid cleanup

--- src/main/java/com/sistema/venus/controller/UserController.java
+++ src/main/java/com/sistema/venus/controller/UserController.java
@@ -1,16 +1,11 @@
 package com.sistema.venus.controller;
 
-import com.sendgrid.*;
-import com.sendgrid.helpers.mail.*;
-import com.sendgrid.helpers.mail.objects.*;
 import com.sistema.venus.repo.UserRepository;
 import com.sistema.venus.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.io.IOException;
 import java.util.Optional;
 
 @RestController
--- src/main/java/com/sistema/venus/services/AuthService.java
+++ src/main/java/com/sistema/venus/services/AuthService.java
@@ -1,12 +1,5 @@
 package com.sistema.venus.services;
 
-import com.sendgrid.Method;
-import com.sendgrid.Request;
-import com.sendgrid.Response;
-import com.sendgrid.SendGrid;
-import com.sendgrid.helpers.mail.Mail;
-import com.sendgrid.helpers.mail.objects.Content;
-import com.sendgrid.helpers.mail.objects.Email;
 import com.sistema.venus.domain.*;
 import com.sistema.venus.util.JwtUtils;
 import org.springframework.beans.factory.annotation.Autowired;
