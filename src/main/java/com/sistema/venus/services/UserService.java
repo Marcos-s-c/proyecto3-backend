652c8b0154f97b93220056fc599ca07db004dda5
Diego Huertas <dhuertasg@ucenfotec.ac.cr>
2023-11-06 22:33:13 -0600

    Encriptación de contraseña

--- src/main/java/com/sistema/venus/services/UserService.java
+++ src/main/java/com/sistema/venus/services/UserService.java
@@ -2,6 +2,7 @@ package com.sistema.venus.services;
 
 import com.sistema.venus.domain.User;
 import com.sistema.venus.repo.UserRepository;
+import com.sistema.venus.util.Utils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -13,9 +14,11 @@ public class UserService implements UserDetailsService {
     @Autowired
     private UserRepository userRepository;
 
+
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
         User user = userRepository.findUserByEmail(email);
+
         return org.springframework.security.core.userdetails.User.builder()
                 .username(user.getEmail())
                 .password(user.getPassword())
