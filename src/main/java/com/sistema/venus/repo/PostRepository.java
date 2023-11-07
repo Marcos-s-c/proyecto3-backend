abe7bd9fe1d95267248a93b87519a1e8449ce46b
Sebastian Quiros <squirosv@ucenfotec.ac.cr>
2023-11-06 21:12:49 -0600

    cloudinary integration

---
+++ src/main/java/com/sistema/venus/repo/PostRepository.java
@@ -0,0 +1,9 @@
+package com.sistema.venus.repo;
+
+import com.sistema.venus.domain.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PostRepository extends JpaRepository<Post,Long> {
+}
