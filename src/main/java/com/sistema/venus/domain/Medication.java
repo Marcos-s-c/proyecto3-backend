8a1feb4d06c825e58d13ad14e719de51e8b91adb
Marcos-s-c <66282209+Marcos-s-c@users.noreply.github.com>
2023-11-10 19:10:41 -0600

    Medicine CRUD

---
+++ src/main/java/com/sistema/venus/domain/Medication.java
@@ -0,0 +1,28 @@
+package com.sistema.venus.domain;
+
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@Table(name = "medications")
+@NoArgsConstructor
+public class Medication {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long medicine_id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="user_id")
+    @JsonBackReference
+    private User userId;
+    private String name;
+    private String dosis;
+    private int frecuencia;
+
+
+}
