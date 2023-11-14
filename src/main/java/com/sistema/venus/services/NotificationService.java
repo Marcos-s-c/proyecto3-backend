b7698f55a1287735349bc00d47591b258ca5925a
Diego Huertas <dhuertasg@ucenfotec.ac.cr>
2023-11-14 14:57:41 -0600

    Se crean alertas de color de flujo y arreglo en la logica de programacion

--- src/main/java/com/sistema/venus/services/NotificationService.java
+++ src/main/java/com/sistema/venus/services/NotificationService.java
@@ -16,7 +16,7 @@ import java.util.List;
 
 @Service
 public class NotificationService {
-
+    boolean save = false;
     @Autowired
     private NotificationsRepository notificationsRepository;
     @Autowired
@@ -32,20 +32,37 @@ public class NotificationService {
             if(notification.getDate()==null){
                 notification.setDate(Utils.getDateCurrentTimezone());
             }
-            notification.setText(createNotificationText(periodCriterias.getValue()));
             notification.setUser_id(user);
             notification.setOpen(false);
-            notificationsRepository.save(notification);
+            notification.setText(createNotificationColor(periodCriterias.getValue()));
+            if(save == true){
+                notificationsRepository.save(notification);
+                save = false;
+            }
+
         }
 
-
+        if(periodCriterias.getFieldName().equals("fluidColor")){
+            Notification notification = new Notification();
+            User user = userRepository.findUserByEmail(SecurityContextHolder.getContext().getAuthentication().getName());
+            if(notification.getDate()==null){
+                notification.setDate(Utils.getDateCurrentTimezone());
+            }
+            notification.setUser_id(user);
+            notification.setText(createNotificationFlujo(periodCriterias.getValue()));
+            notification.setOpen(false);
+            if(save == true){
+                notificationsRepository.save(notification);
+                save = false;
+            }
+        }
     }
 
     public List<Notification> getNotifications(){
         User user = userRepository.findUserByEmail(SecurityContextHolder.getContext().getAuthentication().getName());
-
         return notificationsRepository.getNotificationByUserId(user.getUser_id());
     }
+
 
     public void readNotifications(){
         notificationsRepository.findAll().forEach(notification -> {
@@ -54,21 +71,52 @@ public class NotificationService {
         });
     }
 
-    public String createNotificationText(String color) {
+    public String createNotificationColor(String color) {
         String text = "";
         switch (color) {
             case "rojo palido":
                 text = "Su sangrado fue de color rojo pálido, " +
                         "lo cual puede estar relacionado a la toma de algún anticonceptivo o transtornos hormonales.  " +
                         "Es importante que consulte a su médico de cabecera o ginecólogo de confianza. ";
+                save = true;
                 break;
 
             case "anaranjado":
                 text = "Su sangrado fue de color naranja, lo cual puede estar relacionado infecciones. " +
                         "Es importante que consulte a su médico de cabecera o ginecólogo de confianza. ";
+                save = true;
                 break;
             case "Otro":
                 text = "Su sangrado fue de un color diferente al rojo.  Es importante que consulte a su médico de cabecera o ginecólogo de confianza.";
+                save = true;
+                break;
+
+        }
+        return text;
+    }
+    public String createNotificationFlujo(String color) {
+        String text = "";
+        switch (color) {
+            case "gris":
+                text = "Su flujo cervical/vaginal fue de color gris, puede ser producto de una vaginosis. " +
+                        "Es importante que consulte a su médico de cabecera o ginecólogo de confianza. ";
+                save = true;
+                break;
+
+            case "amarillo":
+                text = "Su flujo cervical/vaginal fue de color amarillo, puede ser producto de una infección ginecológica. " +
+                        "Es importante que consulte a su médico de cabecera o ginecólogo de confianza.";
+                save = true;
+                break;
+            case "verde":
+                text = "Su flujo cervical/vaginal fue de color verde, puede ser producto de una infección ginecológica. " +
+                        "Es importante que consulte a su médico de cabecera o ginecólogo de confianza.";
+                save = true;
+                break;
+            case "transparente":
+                text = "Su flujo cervical/vaginal fue de un color diferente al blanco o transparente. " +
+                        "Es importante que consulte a su médico de cabecera o ginecólogo de confianza.";
+                save = true;
                 break;
 
         }
