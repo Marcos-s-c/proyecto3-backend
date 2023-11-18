1d1f4e5bf348c6a08436698436dec2545b6ce3b4
Sebastian Quiros <squirosv@ucenfotec.ac.cr>
2023-11-18 13:47:08 -0600

    new creds

--- src/main/java/com/sistema/venus/services/TwilioService.java
+++ src/main/java/com/sistema/venus/services/TwilioService.java
@@ -1,18 +1,15 @@
 package com.sistema.venus.services;
 
-import com.sistema.venus.domain.User;
 import com.twilio.Twilio;
 import com.twilio.rest.api.v2010.account.Message;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
 
-/**Account Venus**/
 @Service
 public class TwilioService {
-    public static final String ACCOUNT_SID ="AC08414af662f9787c8f8bab3510aa304d";
-    public static final String AUTH_TOKEN = "6009f3843aec26e9c15443eff47b8afb";
+    public static final String ACCOUNT_SID ="ACa94e1c9dfbd880202efa315da15f551b";
+    public static final String AUTH_TOKEN = "1b01904da78a02c60149f22419273e63";
 
     private UserService userRepository;
 
@@ -20,11 +17,12 @@ public class TwilioService {
     public void setup(){
         Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
     }
+
     public void sendMessage(String phone, String smsMessage){
         Message message = Message.creator(
                         new com.twilio.type.PhoneNumber("+506"+phone),
-                        new com.twilio.type.PhoneNumber("+13343669701"), smsMessage)
+                        //new com.twilio.type.PhoneNumber("+13343669701"), smsMessage)
+                        new com.twilio.type.PhoneNumber("+16785353163"), smsMessage)
                 .create();
-        System.out.println(message.getSid());
     }
 }
