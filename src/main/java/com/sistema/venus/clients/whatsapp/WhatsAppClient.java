0936b197690cf9bfbed0e53665172ba74d37245e
gracielamendezrojas <mmendezr@ucenfotec.ac.cr>
2023-11-19 19:08:43 -0600

    Req-Fun-40-Notificación en Whatsapp del pronóstico del día de inicio de la siguiente menstruación

--- src/main/java/com/sistema/venus/clients/whatsapp/WhatsAppClient.java
+++ src/main/java/com/sistema/venus/clients/whatsapp/WhatsAppClient.java
@@ -1,5 +1,10 @@
 package com.sistema.venus.clients.whatsapp;
 
+import com.fasterxml.jackson.annotation.JsonValue;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.jsonFormatVisitors.JsonAnyFormatVisitor;
+import com.fasterxml.jackson.databind.jsonFormatVisitors.JsonObjectFormatVisitor;
 import com.sistema.venus.clients.whatsapp.request.*;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.*;
@@ -11,12 +16,14 @@ import java.util.List;
 
 @Component
 public class WhatsAppClient {
-    private static final String SEND_MESSAGE_WA_URL ="https://graph.facebook.com/v.17.0/165060313357938/messages";
+    private static final String SEND_MESSAGE_WA_URL ="https://graph.facebook.com/v17.0/165060313357938/messages";
 
     @Value("${whatsapp.token}")
     private String whatsappToken;
 
-    public Boolean sendWAMessage(String toNumber, String text) {
+
+    public Boolean sendWAMessage(String toNumber, String text) throws JsonProcessingException {
+        ObjectMapper mapper = new ObjectMapper();
         RestTemplate restTemplate = new RestTemplate();
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON);
@@ -24,10 +31,10 @@ public class WhatsAppClient {
         WhatsAppMessageTemplate whatsAppMessageTemplate = getWhatsAppMessageTemplate(text);
         WhatsAppMessageRequest whatsAppMessageRequest = new WhatsAppMessageRequest("whatsapp", "+506" + toNumber, "template", whatsAppMessageTemplate);
 
-        HttpEntity<WhatsAppMessageRequest> requestHttpEntity = new HttpEntity<>(whatsAppMessageRequest, headers);
-        ResponseEntity<String> response = restTemplate.exchange(SEND_MESSAGE_WA_URL, HttpMethod.POST, requestHttpEntity, String.class);
-        
-        return response.getStatusCode().is2xxSuccessful();
+        HttpEntity<String> requestHttpEntity = new HttpEntity<>(mapper.writeValueAsString(whatsAppMessageRequest), headers);
+        String response = restTemplate.postForObject(SEND_MESSAGE_WA_URL, requestHttpEntity, String.class);
+
+        return response != null;
     }
 
     private WhatsAppMessageTemplate getWhatsAppMessageTemplate(String text) {
